5f653dbc500451e1f19ae0dcb2a153a656d8bb79
hockey4life63 <Amelancon68@gmail.com>
2017-05-20 17:41:07 -0700

    some firebase setup

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,19 @@
-console.log("hello world");
+$(document).onReady(function() {
+    let database = firebase.database();
+    let userRef = database.ref("/users");
+    let gameRef = database.ref("/games");
+    let blackCardRef = database.ref("/blackCards");
+    let whiteCardREf = database.ref("/whiteCards");
+
+    fireObj = {
+
+
+    }
+
+    makeElement = {
+
+
+    }
+
+
+})
